4c3d7d19ac978aa963c525a7280f097f7087ae5b
Fab9393 <87854039+Fab9393@users.noreply.github.com>
2024-10-04 09:45:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function simulazioneHackingServer(N, M, p, T) {
 // Parametri della simulazione
 let N = 5;  // Numero di server
 let M = 10; // Numero di hacker
-let p = 0.5; // Probabilità di successo (0.5 significa 50% di successo)
+let p = 0.2; // Probabilità di successo (0.5 significa 50% di successo)
 let T = 20; // Numero di simulazioni
 
 // Esegui la simulazione e ottieni i risultati
